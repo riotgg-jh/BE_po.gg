a36e05386deee3a701a31db2fbaaee88fd4dcfcf
hyeyoooo <12qwpo50@gmail.com>
2025-02-21 13:04:59 +0900

    엔티티

---
+++ src/main/java/com/riot/pogg/model/Position.java
@@ -0,0 +1,15 @@
+package com.riot.pogg.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Position {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
---
+++ src/main/java/com/riot/pogg/model/Post.java
@@ -0,0 +1,27 @@
+package com.riot.pogg.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "position_id")
+    private Position position;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
+
+
