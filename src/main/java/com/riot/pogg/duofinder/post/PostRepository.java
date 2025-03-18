44075fe0e273991585aa6cd20589f2b455ef446b
hyeyoooo <12qwpo50@gmail.com>
2025-03-18 15:21:25 +0900

    만료된 게시글 조회

--- src/main/java/com/riot/pogg/duofinder/post/PostRepository.java
+++ src/main/java/com/riot/pogg/duofinder/post/PostRepository.java
@@ -3,11 +3,11 @@ package com.riot.pogg.duofinder.post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findByCategoryName(String categoryName);
-    List<Post> findByPositionName(String positionName);
-}
 
+    List<Post> findByExpirationTimeBefore(LocalDateTime now); // 만료된 게시글 조회
+}
