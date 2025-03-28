90f611ba5a7e5398b348d7b1da5d8437055247b9
hyeyoooo <12qwpo50@gmail.com>
2025-03-28 16:46:16 +0900

    app

--- src/main/java/com/riot/pogg/PoggApplication.java
+++ src/main/java/com/riot/pogg/PoggApplication.java
@@ -2,12 +2,13 @@ package com.riot.pogg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // 스케줄링 활성화
 public class PoggApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PoggApplication.class, args);
 	}
-
 }
